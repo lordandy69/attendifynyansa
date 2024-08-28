27a9676280cf8955a20fa424806a6e14714eaaf7
1realjulius <iamjuliuseghan@gmail.com>
2024-08-28 08:15:10 -0700

    lots of updates

--- app/class/[class_id]/page.tsx
+++ app/class/[class_id]/page.tsx
@@ -1,23 +1,24 @@
-"use client";
-import { useGetClasses } from "@/components/ux/get-classes";
-import { useQRCodeGenerator } from "@/components/ux/qr-code-generator";
-import { FrameContext } from "@/lib/store/FrameContextStore";
-import { format, parseISO, isValid } from "date-fns";
-import { useContext, useEffect, useState } from "react";
-import ExportButton from "@/components/ux/ExportButton";
-import { useGetSession } from "@/lib/supabase/session";
-import { redirect } from "next/navigation";
-import { supabaseClient } from "@/lib/supabase/client";
-import { Database } from "@/types/supabase";
-import { studentsJoinedArray } from "@/types/types";
-import { Button } from "@/components/ui/button";
-import { JoinedStudentsTable } from "@/components/tables/students-joined";
+'use client';
+import { useGetClasses } from '@/components/ux/get-classes';
+import { useQRCodeGenerator } from '@/components/ux/qr-code-generator';
+import { FrameContext } from '@/lib/store/FrameContextStore';
+import { format, parseISO, isValid } from 'date-fns';
+import { useContext, useEffect, useState } from 'react';
+import ExportButton from '@/components/ux/ExportButton';
+import { useGetSession } from '@/lib/supabase/session';
+import { redirect } from 'next/navigation';
+import { supabaseClient } from '@/lib/supabase/client';
+import { Database } from '@/types/supabase';
+import { studentsJoinedArray } from '@/types/types';
+import { Button } from '@/components/ui/button';
+import { JoinedStudentsTable } from '@/components/tables/students-joined';
+import { differenceInDays, isPast } from 'date-fns';
 
 type props = {
   params: { class_id: string };
 };
 
-type classData = Database["public"]["Tables"]["classes"]["Row"];
+type classData = Database['public']['Tables']['classes']['Row'];
 type studentsDetailsProps =
   | {
       created_at: string;
@@ -39,9 +40,9 @@ async function fetchJoinedStudentsDetails(
 
   for (const student of joinedStudents) {
     const { data, error } = await supabase
-      .from("user_profiles")
-      .select("*")
-      .eq("user_id", student.student_id)
+      .from('user_profiles')
+      .select('*')
+      .eq('user_id', student.student_id)
       .single();
 
     if (data) {
@@ -50,6 +51,25 @@ async function fetchJoinedStudentsDetails(
   }
 
   return detailedStudents;
+}
+
+function getClassStatus(classEndDate: string | null): {
+  status: 'Class Ended' | 'Class Upcoming' | 'Date not available';
+} {
+  if (!classEndDate) {
+    return { status: 'Date not available' };
+  }
+
+  const endDate = parseISO(classEndDate);
+  const now = new Date();
+
+  if (isPast(endDate)) {
+    // const daysPassed = differenceInDays(now, endDate);
+    return { status: 'Class Ended' };
+  } else {
+    // const daysRemaining = differenceInDays(endDate, now);
+    return { status: 'Class Upcoming' };
+  }
 }
 
 export default function Page({ params }: props) {
@@ -66,26 +86,26 @@ export default function Page({ params }: props) {
 
   useEffect(() => {
     supabase
-      .from("classes")
-      .select("*")
-      .eq("class_id", params.class_id)
+      .from('classes')
+      .select('*')
+      .eq('class_id', params.class_id)
       .single()
       .then(({ data }) => {
         setClassData(data);
       });
 
     supabase
-      .channel("get-custome-class-updates")
+      .channel('get-custome-class-updates')
       .on(
-        "postgres_changes",
+        'postgres_changes',
         {
-          event: "*",
-          schema: "public",
-          table: "classes",
+          event: '*',
+          schema: 'public',
+          table: 'classes',
           filter: `class_id=eq.${params.class_id}`,
         },
         (payload) => {
-          console.log("Change received!", payload.new);
+          console.log('Change received!', payload.new);
           setClassData(payload.new as any);
         }
       )
@@ -97,17 +117,17 @@ export default function Page({ params }: props) {
       !class_data?.students_joined ||
       class_data.students_joined.length === 0
     ) {
-      return "No student data available";
+      return 'No student data available';
     }
 
     const headers = [
-      "Student Email",
-      "Student ID",
-      "Student Full Name",
-      "Student Index Number",
-      "Student Program",
+      'Student Email',
+      'Student ID',
+      'Student Full Name',
+      'Student Index Number',
+      'Student Program',
     ];
-    const csvRows = [headers.join(",")];
+    const csvRows = [headers.join(',')];
 
     class_data.students_joined.forEach((student: studentsJoinedArray) => {
       const row = [
@@ -117,10 +137,10 @@ export default function Page({ params }: props) {
         student.index_number,
         student.program_name,
       ];
-      csvRows.push(row.join(","));
+      csvRows.push(row.join(','));
     });
 
-    return csvRows.join("\n");
+    return csvRows.join('\n');
   }
 
   // function exportAndDownloadStudentsCSV() {
@@ -140,121 +160,130 @@ export default function Page({ params }: props) {
   // }
 
   const [exportState, setExportState] = useState<
-    "export" | "exporting" | "exported"
-  >("export");
+    'export' | 'exporting' | 'exported'
+  >('export');
 
   function exportAndDownloadStudentsCSV() {
-    setExportState("exporting");
+    setExportState('exporting');
     const csvContent = exportStudentsToCSV();
 
-    const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-    const link = document.createElement("a");
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
     if (link.download !== undefined) {
       const url = URL.createObjectURL(blob);
-      link.setAttribute("href", url);
+      link.setAttribute('href', url);
       link.setAttribute(
-        "download",
+        'download',
         `students_${class_data?.class_id!}_${class_data?.class_name!}.csv`
       );
-      link.style.visibility = "hidden";
+      link.style.visibility = 'hidden';
       document.body.appendChild(link);
 
       setTimeout(() => {
         link.click();
         document.body.removeChild(link);
-        setExportState("exported");
+        setExportState('exported');
 
         // Reset to 'export' after 3 seconds
         setTimeout(() => {
-          setExportState("export");
+          setExportState('export');
         }, 3000);
       }, 1000); // Simulate export process taking 1 second
     }
   }
+  const classStatus = getClassStatus(class_data?.class_end!);
 
   return (
-    <main className="mt-24">
-      <div className="mx-auto max-w-2xl p-4 flex flex-col">
-        <section className="">
-          <div className="grid md:grid-cols-2 gap-4">
-            <div className="flex flex-col space-y-4">
-              <div className="flex flex-col">
-                <p className="text-sm font-medium underline">Class Id</p>
-                <h2 className="text-neutral-500 py-1 px-2 rounded bg-neutral-200 w-fit">
+    <main className='mt-24'>
+      <div className='mx-auto max-w-2xl p-4 flex flex-col'>
+        <section className=''>
+          <div className='grid md:grid-cols-2 gap-4'>
+            <div className='flex flex-col space-y-4'>
+              <div className='flex flex-col'>
+                <p className='text-sm font-medium underline'>Class Id</p>
+                <h2 className='text-neutral-500 py-1 px-2 rounded bg-neutral-200 w-fit'>
                   {class_data?.class_id}
                 </h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">Class Name</p>
-                <h2 className="text-neutral-500">{class_data?.class_name}</h2>
+                <p className='text-sm font-medium underline'>Class Name</p>
+                <h2 className='text-neutral-500'>{class_data?.class_name}</h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">Created At</p>
-                <h2 className="text-neutral-500">
+                <p className='text-sm font-medium underline'>Created At</p>
+                <h2 className='text-neutral-500'>
                   {class_data?.created_at!
-                    ? format(parseISO(class_data?.created_at!), "PPP p")
-                    : "date not available"}
+                    ? format(parseISO(class_data?.created_at!), 'PPP p')
+                    : 'date not available'}
                 </h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">Teacher</p>
-                <h2 className="text-neutral-500">{class_data?.teacher_name}</h2>
+                <p className='text-sm font-medium underline'>Teacher</p>
+                <h2 className='text-neutral-500'>{class_data?.teacher_name}</h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">
+                <p className='text-sm font-medium underline'>
                   Class Start Time
                 </p>
-                <h2 className="text-neutral-500">
+                <h2 className='text-neutral-500'>
                   {class_data?.class_start!
-                    ? format(parseISO(class_data?.class_start!), "PPP p")
-                    : "date not available"}
+                    ? format(parseISO(class_data?.class_start!), 'PPP p')
+                    : 'date not available'}
                 </h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">Class End Time</p>
-                <h2 className="text-neutral-500">
+                <p className='text-sm font-medium underline'>Class End Time</p>
+                <h2 className='text-neutral-500'>
                   {class_data?.class_end!
-                    ? format(parseISO(class_data?.class_end!), "PPP p")
-                    : "date not available"}
+                    ? format(parseISO(class_data?.class_end!), 'PPP p')
+                    : 'date not available'}
                 </h2>
               </div>
               <div>
-                <p className="text-sm font-medium underline">Class Location</p>
-                <h2 className="text-neutral-500">{class_data?.location!}</h2>
+                <p className='text-sm font-medium underline'>Class Location</p>
+                <h2 className='text-neutral-500'>{class_data?.location!}</h2>
               </div>
             </div>
-            <div className="flex flex-col items-center space-y-4">
-              <div
-                ref={frameContext}
-                id="frame"
-                className="flex flex-col items-center space-y-2 bg-white"
-              >
-                <QRCodeComponent
-                  size={346}
-                  qrcodevalue={class_data?.class_id!}
-                />
-                <p>{class_data?.class_name}</p>
-              </div>
-              <div>
-                <ExportButton />
-              </div>
+            <div className='flex flex-col py-4'>
+              {classStatus.status === 'Class Upcoming' ? (
+                <div className='flex flex-col items-center space-y-4'>
+                  <div
+                    ref={frameContext}
+                    id='frame'
+                    className='flex flex-col items-center space-y-2 bg-white'
+                  >
+                    <QRCodeComponent
+                      size={346}
+                      qrcodevalue={class_data?.class_id!}
+                    />
+                    <p>{class_data?.class_name}</p>
+                  </div>
+                  <div>
+                    <ExportButton />
+                  </div>
+                </div>
+              ) : classStatus.status == 'Class Ended' ? (
+                <p className='text-4xl font-medium'>Class Has Ended</p>
+              ) : (
+                <p className='text-4xl'>Date Not Available</p>
+              )}
             </div>
           </div>
-          <div className="flex flex-col my-10 border-t items-center border-neutral-200">
-            <div className="flex flex-col w-full">
+          <div className='flex flex-col my-10 border-t items-center border-neutral-200'>
+            <div className='flex flex-col w-full'>
               {!class_data?.students_joined ? (
-                <p className="">No Students have Joined</p>
+                <p className=''>No Students have Joined</p>
               ) : (
-                <div className="flex flex-col">
-                  <div className="flex flex-col md:flex-row items-center space-x-4 py-4 justify-between">
-                    <p className="mb-2">Studens Who Joined</p>
+                <div className='flex flex-col'>
+                  <div className='flex flex-col md:flex-row items-center space-x-4 py-4 justify-between'>
+                    <p className='mb-2'>Studens Who Joined</p>
                     <Button
                       onClick={exportAndDownloadStudentsCSV}
-                      disabled={exportState === "exporting"}
+                      disabled={exportState === 'exporting'}
                     >
-                      {exportState === "export" && "Export Students CSV"}
-                      {exportState === "exporting" && "Exporting..."}
-                      {exportState === "exported" && "Exported!"}
+                      {exportState === 'export' && 'Export Students CSV'}
+                      {exportState === 'exporting' && 'Exporting...'}
+                      {exportState === 'exported' && 'Exported!'}
                     </Button>
                   </div>
                   <JoinedStudentsTable data={class_data.students_joined} />
--- app/class/page.tsx
+++ app/class/page.tsx
@@ -1,57 +1,160 @@
-"use client";
-import { buttonVariants } from "@/components/ui/button";
-import { useGetAllClasses } from "@/components/ux/get-classes";
-import { useGetSession } from "@/lib/supabase/session";
-import { cn } from "@/lib/utils";
-import { format, parseISO } from "date-fns";
-import Link from "next/link";
-import { redirect, useRouter } from "next/navigation";
-
+'use client';
+import { buttonVariants } from '@/components/ui/button';
+import { useGetAllClasses } from '@/components/ux/get-classes';
+import { useGetSession } from '@/lib/supabase/session';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { redirect, useRouter } from 'next/navigation';
+import {
+  isSameDay,
+  isPast,
+  isFuture,
+  parseISO,
+  startOfDay,
+  format,
+} from 'date-fns';
+import { Database } from '@/types/supabase';
+import { useEffect, useState } from 'react';
+type classData = Database['public']['Tables']['classes']['Row'];
+type classCat = {
+  today: classData[];
+  upcoming: classData[];
+  ended: classData[];
+};
 export default function Page() {
-  const { profile, user } = useGetSession();
   const router = useRouter();
+  const [classCategories, setClassCategories] = useState<classCat>();
+  const { profile, user } = useGetSession();
   const { all_class_data } = useGetAllClasses(profile?.user_id!);
+  const today = classCategories?.today!;
+  const upcoming = classCategories?.upcoming!;
+  const ended = classCategories?.ended!;
+  function categorizeClasses(classes: classData[]): {
+    today: classData[];
+    upcoming: classData[];
+    ended: classData[];
+  } {
+    const now = startOfDay(new Date());
+
+    return classes.reduce(
+      (acc, classItem) => {
+        const startDate = classItem.class_start
+          ? parseISO(classItem.class_start)
+          : null;
+        const endDate = classItem.class_end
+          ? parseISO(classItem.class_end)
+          : null;
+
+        if (startDate && isSameDay(startDate, now)) {
+          acc.today.push(classItem);
+        } else if (startDate && isFuture(startDate)) {
+          acc.upcoming.push(classItem);
+        } else if (endDate && isPast(endDate)) {
+          acc.ended.push(classItem);
+        }
+
+        return acc;
+      },
+      { today: [], upcoming: [], ended: [] } as {
+        today: classData[];
+        upcoming: classData[];
+        ended: classData[];
+      }
+    );
+  }
+  useEffect(() => {
+    if (all_class_data) {
+      const categorizedClasses = categorizeClasses(all_class_data);
+      setClassCategories(categorizedClasses);
+    }
+  }, [all_class_data]);
 
   return (
     <main className='mt-24'>
-      <div className='mx-auto max-w-2xl p-4 flex flex-col'>
-        <section className=''>
-          <div className='flex flex-col w-full items-center justify-center'>
-            <Link
-              href={"/new-class"}
-              className={cn(
-                buttonVariants({}),
-                "bg-gradient-to-r from-[#3A608A] to-[#232323]"
-              )}
-            >
-              Create New Class
-            </Link>
-          </div>
-          <div className='flex flex-col my-10 border-t items-center border-neutral-200'>
-            {!all_class_data ? (
-              <p className='py-10'>No Classes ...Create New Class</p>
-            ) : (
-              <div className='flex flex-col space-y-4 my-4  '>
-                {all_class_data.map((c) => {
-                  return (
-                    <Link
-                      href={`/class/${c.class_id}`}
-                      key={c.id}
-                      className='flex items-center justify-between border border-neutral-200 rounded-xl px-4 py-2'
-                    >
-                      <div>
-                        <p>{c.class_name}</p>
-                        <p className='text-sm'>{c.teacher_name}</p>
-                      </div>
-                      <p className=''>open class</p>
-                    </Link>
-                  );
-                })}
-              </div>
+      <div className='mx-auto max-w-2xl p-4 w-full flex flex-col'>
+        <div className='flex flex-col w-full items-center justify-center'>
+          <Link
+            href={'/new-class'}
+            className={cn(
+              buttonVariants({}),
+              'bg-gradient-to-r from-[#3A608A] to-[#232323]'
             )}
-          </div>
-        </section>
+          >
+            Create New Class
+          </Link>
+        </div>
+        <div className='flex flex-col my-10 border-t w-full border-neutral-200'>
+          {!all_class_data ? (
+            <p className='py-10'>No Classes ...Create New Class</p>
+          ) : (
+            <div className='flex flex-col space-y-6 my-6  '>
+              <div className='flex flex-col w-full divide-y divide-neutral-200'>
+                <p className='text-xl font-medium py-2'>Today Classes</p>
+                <div>
+                  {!today ? (
+                    <p>No Classes Today</p>
+                  ) : (
+                    <div className='divide-y divide-neutral-200 flex flex-col *:py-2'>
+                      {today.length == 0 && <p>No Classes Today</p>}
+                      {today.map((c) => (
+                        <ClassComp c={c} />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className='flex flex-col w-full divide-y divide-neutral-200'>
+                <p className='text-xl font-medium py-2'>Upcoming Classes</p>
+                <div>
+                  {!upcoming ? (
+                    <p>No Upcoming Classes</p>
+                  ) : (
+                    <div className='divide-y divide-neutral-200 flex flex-col *:py-2'>
+                      {upcoming.length == 0 && <p>No Upcoming Classes</p>}
+                      {upcoming.map((c) => (
+                        <ClassComp c={c} />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className='flex flex-col w-full divide-y divide-neutral-200'>
+                <p className='text-xl font-medium py-2'>Ended Classes</p>
+                <div>
+                  {!ended ? (
+                    <div>No Classes Today</div>
+                  ) : (
+                    <div className='divide-y divide-neutral-200 flex flex-col *:py-2'>
+                      {upcoming.length == 0 && <p>No Ended Classes</p>}
+                      {ended.map((c) => (
+                        <ClassComp c={c} />
+                      ))}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </main>
+  );
+}
+
+function ClassComp({ c }: { c: classData }) {
+  return (
+    <div className='flex items-center justify-between'>
+      <div className='flex flex-col'>
+        <p>{c.class_name}</p>
+        <p className='text-sm'>
+          {c.teacher_name} <br /> {format(parseISO(c.created_at!), 'PPP p')}
+        </p>
+      </div>
+      <div>
+        <Link href={`/class/${c.class_id}`} className={cn(buttonVariants({}))}>
+          View Class
+        </Link>
+      </div>
+    </div>
   );
 }
